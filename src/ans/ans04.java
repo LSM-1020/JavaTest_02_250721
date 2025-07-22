9c1ffd4c23312c0fe35f341a49ae795056786e38
LSM-1020 <smmlee@naver.com>
2025-07-22 10:09:38 +0900

    풀이 완성

--- src/ans/ans04.java
+++ src/ans/ans04.java
@@ -4,7 +4,13 @@ public class ans04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// 아래 배열에서 홀수인덱스의 값만 출력하시오. {10, 20, 30, 40, 50}
+		int[] numArr = {10,20,30,40,50};
+		for(int i=0;i<numArr.length;i++) {
+			if(i % 2 == 1) {
+				System.out.println(numArr[i]);
+			}
+		}
 	}
 
 }
--- src/ans/ans05.java
+++ src/ans/ans05.java
@@ -4,7 +4,17 @@ public class ans05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//아래 숫자들을 모두 곱한 값을 배열을 사용하여 출력하시오. 10 20 30 40 50
+		int[] numArr = {1,2,3,4,5};
+		int num = 1;
+		
+		for(int sum : numArr) {
+			
+			num = num * sum;
+			
+			
+		}
+		System.out.println(num);
 	}
 
 }
